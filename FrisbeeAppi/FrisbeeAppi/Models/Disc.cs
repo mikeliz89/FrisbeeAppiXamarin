535f802ccab87783c2f498ad0ad7dcfab5ad74fb
Miika Kontio <miika.kontio@innofactor.com>
2020-09-30 18:04:12 +0300

    Kiekon model lisätty

---
+++ FrisbeeAppi/FrisbeeAppi/Models/Disc.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System;
+
+namespace FrisbeeAppi.Models
+{
+    /// <summary>
+    /// Kiekko
+    /// </summary>
+    public class Disc
+    {
+				[PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+				/// <summary>
+		    /// Kiekko lisätty kokoelmaan pvm
+		    /// </summary>
+		    public DateTime Created { get; set; }
+        /// <summary>
+        /// Kiekon nimi
+        /// </summary>
+        public string Name { get; set; }
+				/// <summary>
+		    /// Valmistaja
+		    /// </summary>
+				public string Manufacturer { get; set; }
+		    /// <summary>
+		    /// Malli
+		    /// </summary>
+		    public string Model { get; set; }
+				/// <summary>
+		    /// Lyhyt kuvaus
+		    /// </summary>
+		    public string Description { get; set; }
+        /// <summary>
+		    /// Nopeus
+		    /// </summary>
+		    public double Speed { get; set; }
+				/// <summary>
+		    /// Liito
+		    /// </summary>
+		    public double Glide { get; set; }
+				/// <summary>
+		    /// Vakaus
+		    /// </summary>
+		    public double Turn { get; set; }
+				/// <summary>
+		    /// Loppufeidi
+		    /// </summary>
+		    public double Fade { get; set; }
+		}
+}
